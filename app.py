ac7b140283dff6f3f89182a5e6db396166598d04
vjstudio2022-art <vjstudio2022@gmail.com>
2026-03-24 18:57:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -211,4 +211,5 @@ def logout():
     session.clear(); return redirect(url_for("home"))
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+   port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
